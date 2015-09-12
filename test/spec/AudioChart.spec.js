285db909b7762226a553c416d77f65756f9ec303
Matthew Tylee Atkinson <matatk@gmail.com>
2015-09-12 11:52:54 +0200

    Bits and bobs on moving to Jasmine 2 and tidying up
    * Fix most of the tests in WebAudioSounder to work with Jasmine 2.
    * Start moving to tabs rather than spaces for indentation.

--- test/spec/AudioChart.spec.js
+++ test/spec/AudioChart.spec.js
@@ -1,26 +1,26 @@
 describe('AudioChart', function() {
-  var fake_options = null;
+	var fake_options = null;
 
-  beforeEach(function() {
-    fake_options = { type: 'test' };
-  });
+	beforeEach(function() {
+		fake_options = { type: 'test' };
+	});
 
-  it('passes its context to _AudioChart', function() {
-    function FakeAudioContext() {}
-    var fake_audio_context = new FakeAudioContext();
-    spyOn(window, '_AudioChart');
+	it('passes its context to _AudioChart', function() {
+		function FakeAudioContext() {}
+		var fake_audio_context = new FakeAudioContext();
+		spyOn(window, '_AudioChart');
 
-    var audiochart = new window.AudioChart(fake_options, fake_audio_context);
-    expect(window._AudioChart)
-      .toHaveBeenCalledWith(fake_options, fake_audio_context);
-  });
+		var audiochart = new window.AudioChart(fake_options, fake_audio_context);
+		expect(window._AudioChart)
+			.toHaveBeenCalledWith(fake_options, fake_audio_context);
+	});
 
-  it('can create a new context and pass it to _AudioChart', function() {
-    spyOn(window, '_AudioChart');
-	spyOn(window.AudioContextGetter, 'get').and.returnValue(42);
+	it('can create a new context and pass it to _AudioChart', function() {
+		spyOn(window, '_AudioChart');
+		spyOn(window.AudioContextGetter, 'get').and.returnValue(42);
 
-    var audiochart = new window.AudioChart(fake_options);
-    expect(window._AudioChart)
-      .toHaveBeenCalledWith(fake_options, 42);
-  });
+		var audiochart = new window.AudioChart(fake_options);
+		expect(window._AudioChart)
+			.toHaveBeenCalledWith(fake_options, 42);
+	});
 });
--- test/spec/AudioContextGetter.spec.js
+++ test/spec/AudioContextGetter.spec.js
@@ -1,10 +1,10 @@
 describe('AudioContextGetter', function() {
-  it('returns the same audio context when called again', function() {
-    var context1, context2, context3;
-    context1 = window.AudioContextGetter.get();
-    context2 = window.AudioContextGetter.get();
-    context3 = window.AudioContextGetter.get();
-    expect(context1).toBe(context2);
-    expect(context2).toBe(context3);
-  });
+	it('returns the same audio context when called again', function() {
+		var context1, context2, context3;
+		context1 = window.AudioContextGetter.get();
+		context2 = window.AudioContextGetter.get();
+		context3 = window.AudioContextGetter.get();
+		expect(context1).toBe(context2);
+		expect(context2).toBe(context3);
+	});
 });
