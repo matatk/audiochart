364b00cff7fcfb5d0378fb6ab2fd3b3a38f39fa5
Matthew Tylee Atkinson <matatk@gmail.com>
2015-09-16 10:35:02 +0100

    Improving structure and tidiness
    * Add the following to _layouts/base.html (for DRYness):
      - extra-style
      - extra-script
    * Use the above to split up the CSS and load apt scripts.
    * Use tabs in Gruntfile.
    * Remove spurious tab in index.html.
    * Remove space-before-EOL from minified code.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,51 +1,51 @@
 module.exports = function(grunt) {
-  require('load-grunt-tasks')(grunt);
-  require('time-grunt')(grunt);
+	require('load-grunt-tasks')(grunt);
+	require('time-grunt')(grunt);
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 
-    clean: {
-      lib: 'lib/'
-    },
+		clean: {
+			lib: 'lib/'
+		},
 
-    jshint: {
-      all: ['Gruntfile.js', 'src/*.js', 'test/spec/*.js']
-    },
+		jshint: {
+			all: ['Gruntfile.js', 'src/*.js', 'test/spec/*.js']
+		},
 
-    jasmine: {
-      src: 'src/<%= pkg.name %>.js',
-      options: {
-        specs: 'test/spec/*.spec.js',
-        outfile: 'test/index.html',
-        keepRunner: true
-      }
-    },
+		jasmine: {
+			src: 'src/<%= pkg.name %>.js',
+			options: {
+				specs: 'test/spec/*.spec.js',
+				outfile: 'test/index.html',
+				keepRunner: true
+			}
+		},
 
-    uglify: {
-      lib: {
-        options: {
-          beautify: true,
-          mangle: false,
-          banner: '/* <%= pkg.name %> ' +
-            '<%= grunt.template.today("yyyy-mm-dd") %> \n' +
-            '<%= pkg.license %> licence */\n',
-          sourceMap: true,
-          enclose: {
-            'window': 'exports'
-          }
-        },
-        files: {
-          'lib/<%= pkg.name %>.min.js': 'src/*.js'
-        }
-      }
-    }
-  },
+		uglify: {
+			lib: {
+				options: {
+					beautify: true,
+					mangle: false,
+					banner: '/* <%= pkg.name %> ' +
+						'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+						'<%= pkg.license %> licence */\n',
+					sourceMap: true,
+					enclose: {
+						'window': 'exports'
+					}
+				},
+				files: {
+					'lib/<%= pkg.name %>.min.js': 'src/*.js'
+				}
+			}
+		}
+	},
 
-  grunt.registerTask('default', [
-    'clean',
-    'jshint',
-    'jasmine',
-    'uglify'
-  ]));
+	grunt.registerTask('default', [
+		'clean',
+		'jshint',
+		'jasmine',
+		'uglify'
+	]));
 };
