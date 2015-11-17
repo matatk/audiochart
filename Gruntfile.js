62db3253cbb91abc579438cd52140fa1b2bd6564
Matthew Tylee Atkinson <matatk@gmail.com>
2015-11-17 22:22:58 +0000

    Prepare for HTML fixture tests
    * Grab jasmine-jquery and jquery in Gruntfile (using
      grunt-if-missing and grunt-curl).
    * Include the libraries in the repo (for gh-pages).
    * `grunt clean` will remove lib/* and jasmine-jquery
      (and thus jquery too).
    * The clean that is done as part of normal grunt usage
      will not remove the jquery and jasmine-jquery code.
    * Update package.json accordingly.
    
    Note: tried Bower but it was OTT for what's needed here.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,22 @@ module.exports = function(grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 
 		clean: {
-			lib: 'lib/*'
+			lib: 'lib/*',
+			jasmine_jquery: [
+				'test/jquery.js',
+				'test/jasmine-jquery.js'
+			]
+		},
+
+		curl: {
+			jquery: {
+				src: 'http://code.jquery.com/jquery-1.11.3.min.js',
+				dest: 'test/jquery.js'
+			},
+			jasmine_jquery: {
+				src: 'https://raw.github.com/velesin/jasmine-jquery/master/lib/jasmine-jquery.js',
+				dest: 'test/jasmine-jquery.js'
+			}
 		},
 
 		jshint: {
@@ -16,6 +31,10 @@ module.exports = function(grunt) {
 		jasmine: {
 			src: 'src/<%= pkg.name %>.js',
 			options: {
+				vendor: [
+					'test/jquery.js',
+					'test/jasmine-jquery.js'
+				],
 				specs: 'test/spec/*.spec.js',
 				outfile: 'test/index.html',
 				keepRunner: true
@@ -40,12 +59,14 @@ module.exports = function(grunt) {
 				}
 			}
 		}
-	},
+	});
 
 	grunt.registerTask('default', [
-		'clean',
+		'clean:lib',
+		'if-missing:curl:jquery',
+		'if-missing:curl:jasmine_jquery',
 		'jshint',
 		'jasmine',
 		'uglify'
-	]));
+	]);
 };
