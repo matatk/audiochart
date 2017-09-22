e842809f774a6d4ee0655b2e1918c81c77111f27
Matthew Tylee Atkinson <matatk@gmail.com>
2017-09-22 21:21:44 +0100

    Reflect the move to npm scripts in grab.sh

--- grab.sh
+++ grab.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 PROG=`basename $0`
 AC='audiochart.master'
-GRUNT='.grunt'
 LIB='lib'
-TEST='test'
+COVERAGE='coverage'
 EXAMPLES='examples'
 EXAMPLES_DOCCO_FONTS='examples/hello/public'
 ESLINTRC='.eslintrc.json'
 DOC_ROOT='doc'
 
 echo "Removing existing stuff..."
-rm -rfv $GRUNT $LIB $TEST $EXAMPLES $DOC_ROOT
+rm -rfv $LIB $COVERAGE $EXAMPLES $DOC_ROOT
 
 if [ $PROG == 'grab.sh' ]; then
 	if [ ! -f "$AC/package.json" ]; then
@@ -27,19 +26,19 @@ if [ $PROG == 'grab.sh' ]; then
 	cd $AC || exit 42
 	git pull || exit 42
 	npm install || exit 42
-	grunt || exit 42
+	npm run build || exit 42
 	cd -
 	echo
 	echo "Coyping in needed things..."
-	cp -R \
-		"$AC/$GRUNT" \
+	cp -Rv \
 		"$AC/$LIB" \
-		"$AC/$TEST" \
+		"$AC/$COVERAGE" \
 		"$AC/$EXAMPLES" \
 		"$AC/$DOC_ROOT" \
 		. || exit 42
+	echo
 	echo "Removing some development/unused files..."
-	rm -fv "$TEST/spec/$ESLINTRC" "$EXAMPLES/$ESLINTRC" || exit 42
+	rm -fv "$EXAMPLES/$ESLINTRC" || exit 42
 	rm -rfv "$EXAMPLES_DOCCO_FONTS" || exit 42
 	echo
 	echo Updating index.md from README.md in master branch...
